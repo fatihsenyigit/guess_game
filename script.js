e14829f5ba8a2c56ecf6509d45dc8131b6cdc29d
fatih <fsenyigit8@gmail.com>
2024-03-05 12:20:43 +0300

    some update

--- script.js
+++ script.js
@@ -8,24 +8,24 @@ let guess = +prompt(`Guess a number between 1 - 100`);
 
 while (attempts > 0 && attempts <= 3) {
   if (guess < 0 || guess > 100) {
-    console.log("Please enter a number between 0 and 100");
+    alert("Please enter a number between 0 and 100");
     guess = +prompt("Please enter a number (0-100)");
   } else if (guess > number) {
-    console.log("Wrong number, please enter lower number");
+    alert("Wrong number, please enter lower number");
     --attempts;
-    console.log(` You have ${attempts} attempts left`);
+    alert(` You have ${attempts} attempts left`);
     guess = +prompt("Please enter a number (0-100)");
   } else if (guess < number) {
-    console.log("Wrong number, please enter higher number");
+    alert("Wrong number, please enter higher number");
     --attempts;
-    console.log(` You have ${attempts} attempts left`);
+    alert(` You have ${attempts} attempts left`);
     guess = +prompt("Please enter a number (0-100)");
   } else {
-    console.log("YOU WON!!");
+    alert("YOU WON!!");
     break;
   }
   if (attempts == 0) {
-    console.log("Game Over!!");
+    alert("Game Over!!");
   }
 }
 
